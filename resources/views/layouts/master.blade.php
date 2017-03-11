fbf82c6155359e4aef5ad0eb72045ebe2885c8cc
PrabodhaNirmani <prabodhanh@gmail.com>
2017-03-12 00:22:33 +0530

    add new change

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        @yield('title')
+
+    </title>
+    @yield('header')
+    @yield('style')
+
+</head>
+<body>
+    @yield('body')
+
+
+    @yield('javascript')
+</body>
+</html>
